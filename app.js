aee24c5107fedb60f3839bc28e5b6fd32d5ecf8e
teroz <terence.namusonge@gmail.com>
2021-10-27 11:27:46 +0200

    renamed index to app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+
+var express = require('express');
+var requestify = require('requestify');
+
+
+const { App, ExpressReceiver,LogLevel } = require('@slack/bolt');
+
+// Create a Bolt Receiver
+const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET });
+
+// Create the Bolt App, using the receiver
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  receiver,
+  logLevel: LogLevel.DEBUG
+});
+
+app.use(express.urlencoded({ extended: false}))
+
+receiver.router.post('/weather', (req, res) => {
+  city = encodeURIComponent(req.body.city);
+  requestify.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.APPID}&units=metric`)
+  .then(function(result) {
+      res.send(result.getBody().main)
+  })
+  .catch(function(err){
+    res.send(err)
+  })  
+});
+
+
+(async () => {
+  await app.start(3000);
+  console.log('app is running');
+})();
